8bed12f365e457a0f206c3107f128f8ae1daea2a
jairox01 <j4iro.a@gmail.com>
2024-10-01 17:54:53 -0500

    updatebase path on fonts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/raiz-mapp-app/' : '',
-  };
-  
-  module.exports = nextConfig;
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/raiz-mapp-app/' : '',
+};
+
+module.exports = nextConfig;
