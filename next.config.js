147b877b44562503773da0177d4db1d03ec81094
jairox01 <j4iro.a@gmail.com>
2024-10-01 16:31:33 -0500

    update base path config 2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  assetPrefix: isProd ? '/raiz-mapp-app/' : '',
-};
-
-module.exports = nextConfig;
+    output: 'export',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/raiz-mapp-app/' : '',
+  };
+  
+  module.exports = nextConfig;
