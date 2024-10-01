affd7fc96b7ad68251efbf4062c4ab1a68d442fe
JairoX <jairo@fanxielab.com>
2024-10-01 14:40:44 -0500

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  assetPrefix: isProd ? '/raiz-map-app/' : '',
+};
+
 export default nextConfig;
