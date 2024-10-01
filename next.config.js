21f40808cc51b9f10c6c9872446137c671a12bbb
jairox01 <j4iro.a@gmail.com>
2024-10-01 16:09:13 -0500

    update build rules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/raiz-map-app/' : '',
   };
   
-export default nextConfig;
+  module.exports = nextConfig; // Usa `module.exports` en lugar de `export default`
+  
