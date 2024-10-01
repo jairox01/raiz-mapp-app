05d065bf49de8810286cab3fd8e3ac5540f2fdcd
JairoX <jairo@fanxielab.com>
2024-10-01 13:56:34 -0500

    set up base project

---
+++ src/app/components/map.js
@@ -0,0 +1,115 @@
+"use client"; // Agrega esta línea al principio del archivo
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import axios from 'axios';
+
+const mapContainerStyle = {
+  height: '1000px',
+};
+
+const center = {
+  lat: 0.9214413,
+  lng: -77.4354531,
+};
+
+const MapComponent = () => {
+  const [markers, setMarkers] = useState([]);
+  const [selectedMarker, setSelectedMarker] = useState(null);
+
+  useEffect(() => {
+    const fetchMarkers = async () => {
+      const spreadsheetId = '1YpG6mjwNuiSApHwJafqaiGnNccAE746gk56Z1SJc4Gg'; // Reemplaza con el ID de tu Spreadsheet
+      const apiKey = 'AIzaSyByc4JiGVTQCH4w-tPZWnNfVyjcgAjuBjo'; // Reemplaza con tu API Key
+      const range = 'FUNES!A:N'; // Cambia el rango según la estructura de tu hoja
+
+      try {
+        const response = await axios.get(
+          `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`
+        );
+
+        const formattedMarkers = response.data.values.map(item => ({
+          group: item[0],
+          specie: item[1],
+          origin: item[2],
+          taxon: item[3],
+          status: item[4],
+          family: item[5],
+          shortDescription: item[6],
+          longDescription: item[7],
+          latitude: item[8],
+          longitude: item[9],
+          altitude: item[10],
+          position: { lat: parseFloat(item[11]), lng: parseFloat(item[12]) },          
+        }));
+        setMarkers(formattedMarkers);
+      } catch (error) {
+        console.error("Error fetching markers: ", error);
+      }
+    };
+    fetchMarkers();
+  }, []);
+
+  const handleMarkerClick = (marker) => {
+    setSelectedMarker(marker);    
+  };
+
+  return (
+    <div>
+      <LoadScript googleMapsApiKey="AIzaSyByc4JiGVTQCH4w-tPZWnNfVyjcgAjuBjo">
+        <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={12}>
+          {markers.map((marker, index) => (
+            <Marker
+              key={index}
+              position={marker.position}
+              onClick={() => handleMarkerClick(marker)}
+            />            
+          ))}
+
+          {selectedMarker && (
+            <InfoWindow
+            position={selectedMarker.position}
+              onCloseClick={() => setSelectedMarker(null)}  // Cierra el InfoWindow
+            >
+              <div class="info-window">
+                <h5>{selectedMarker.specie}</h5>
+                <img src="/images/pajaro-quemamais.png"/>
+                <p><strong>Familia: </strong>{selectedMarker.family}</p>
+                <a onClick={() => {
+                  const element = document.getElementById("info-box");
+                  if (element) {
+                    element.scrollIntoView({ behavior: "smooth" });
+                  }
+                }}>Ver más</a>
+              </div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
+        <div>
+          {selectedMarker && (
+            <div className={'card'} id="info-box">
+              <div class="columns">
+                <div class="column">
+                  <img src="/images/pajaro-quemamais.png"/>
+                  <ul>
+                    <li><strong>Latitud:</strong> {selectedMarker.latitude}</li>
+                    <li><strong>Longitud:</strong> {selectedMarker.longitude}</li>
+                    <li><strong>Altitud:</strong> {selectedMarker.altitude}</li>
+                    <li><strong>Distribución del taxón:</strong> {selectedMarker.taxon}</li>
+                    <li><strong>Estado de conservación:</strong> {selectedMarker.status}</li>
+                  </ul>
+                </div>
+                <div class="column">
+                  <h3>{selectedMarker.specie}</h3>
+                  <h5>Descripción</h5>
+                  <p>{selectedMarker.longDescription}</p>
+                </div>            
+              </div>  
+            </div>                  
+          )}
+        </div>       
+      </LoadScript>
+    </div>    
+  );
+};
+
+export default MapComponent;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,6 @@
 import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "../styles/main.scss";
+import Image from "next/image";
 
 export const metadata = {
   title: "Create Next App",
@@ -19,9 +9,50 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en">     
+      <body>    
+        <header className="site-header">
+          <div className="container">
+            <div className="columns">
+            <div className="site-header__branding column">
+              <a href="/" className="site-header__branding-link">
+                <Image src="/images/oikos_logo.png" alt="Oikos Logo" width={256} height={101} />
+              </a>
+            </div>
+            <div className="site-header__menu">
+              <ul>
+                <li><a className="active" href="/">Inicio</a></li>
+                <li><a href="/">Contáctanos</a></li>
+              </ul>
+            </div>
+            </div>            
+          </div>
+        </header>
         {children}
+        <footer> 
+          <div className="container">
+            <div className="columns">
+              <div className="column">
+                <h4>Nuestros aliados</h4>
+              </div>            
+            </div>
+            <div className="columns">              
+              <div className="column">
+                <div className="sposors">
+                    <a href="">
+                      <img src="/images/aics.png" alt="Aliado 1" />
+                    </a>
+                    <a href="">
+                      <img src="/images/cevi.png" alt="Aliado 2" />
+                    </a>
+                    <a href="">
+                      <img src="/images/isais.png" alt="Aliado 3" width={120} height={100} />
+                    </a>
+                </div>
+              </div>              
+            </div>
+          </div>
+        </footer>
       </body>
     </html>
   );
