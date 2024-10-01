611a51753574f3fae7a3f911aaabf9ba2d624130
jairox01 <jairox01@users.noreply.github.com>
2024-10-01 21:18:40 +0000

    deploy: 9fa9b8a806a9dcd2a8b57b73a57fb83a34d67d9c

---
+++ _next/static/chunks/app/layout-df7587b7410da20c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5054:function(n,e,t){Promise.resolve().then(t.t.bind(t,3605,23)),Promise.resolve().then(t.t.bind(t,9459,23))},9459:function(){}},function(n){n.O(0,[605,428,526,744],function(){return n(n.s=5054)}),_N_E=n.O()}]);
