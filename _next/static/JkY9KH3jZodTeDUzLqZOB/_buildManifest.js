cceb42504291943c6b963da7e07a07a98ea93bf6
jairox01 <jairox01@users.noreply.github.com>
2024-10-18 20:07:18 +0000

    deploy: 4a2cad2932085cf5f507b43b13ea3b2ce5460810

--- _next/static/JkY9KH3jZodTeDUzLqZOB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-3eb0af671e346c2f.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/chunks/app/contact/page-fa162821d1b56a06.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[327,301],{7984:function(){}},function(n){n.O(0,[428,526,744],function(){return n(n.s=7984)}),_N_E=n.O()}]);
